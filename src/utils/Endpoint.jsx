895a953c39a777233cfcbbd6d3f4ca3cafae833c
Akayuki Kobayashi <148461661+Akayuki-Project@users.noreply.github.com>
2025-04-20 12:31:07 +0700

    Update Endpoint.jsx

--- src/utils/Endpoint.jsx
+++ src/utils/Endpoint.jsx
@@ -1,4 +1,4 @@
-const BASEURL = "http://localhost:4000";
+const BASEURL = import.meta.env.VITE_API_URL;
 
 export const URL_SIGNIN = `${BASEURL}/api/auth/signin`;
 export const URL_PRODUCT = `${BASEURL}/api/products`;
