c217b3d4c058453ff3c34fcd2e2fc96ab103912f
Akayuki Kobayashi <148461661+Akayuki-Project@users.noreply.github.com>
2025-04-20 12:45:02 +0700

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -35,7 +35,7 @@ const Checkout = () => {
       return;
     }
 
-    const finalPrice = product.price || 0;
+    const finalPrice = product.price;
 
     const data = {
       first_name: values.first_name,
